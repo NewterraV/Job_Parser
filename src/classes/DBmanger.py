441ffd8dc17d23687e38485c0830fe5b58586240
Vladislav Poroshin <vld.poroshin@gmail.com>
2023-07-10 14:41:51 +0400

    Добавлен метод get_vacancies_with_higher_salary, который получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.

--- src/classes/DBmanger.py
+++ src/classes/DBmanger.py
@@ -55,6 +55,7 @@ class DBmanager:
                     salary_to INTEGER,
                     currency VARCHAR(5),
                     area VARCHAR(70),
+                    created DATE,
                     url TEXT,
                     requirement VARCHAR(255)
                     )
@@ -82,7 +83,7 @@ class DBmanager:
         conn.commit()
         conn.close()
 
-    def save_data_to_vacancies(self, data):
+    def save_data_to_vacancies(self, data) -> None:
         """Метод добавляет данные в таблицу vacancies"""
 
         # Открытие соединения с базой данных
@@ -93,14 +94,15 @@ class DBmanager:
             for item in tqdm(data, colour='green'):
                 cur.execute("""
                             INSERT INTO vacancies 
-                            (employer_id, title, salary_from, salary_to, currency, area, url, requirement)
-                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                            (employer_id, title, salary_from, salary_to, currency, area, created, url, requirement)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                         """, (item['employer_id'],
                               item['name'],
                               item['salary']['from'],
                               item['salary']['to'],
                               item['salary']['currency'],
                               item['area'],
+                              item['created'],
                               item['url'],
                               item["requirement"]
                               ))
@@ -109,7 +111,7 @@ class DBmanager:
         conn.commit()
         conn.close()
 
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self) -> list[dict]:
         """Метод получает список всех компаний и количество вакансий у каждой компании"""
 
         # Открытие соединения с базой данных
@@ -132,7 +134,7 @@ class DBmanager:
         conn.close()
         return data
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(self) -> list[dict]:
         """Метод получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию."""
 
@@ -156,11 +158,65 @@ class DBmanager:
         conn.close()
         return data
 
-    def get_avg_salary(self):
-        pass
+    def get_avg_salary(self) -> float:
+        """Метод получает среднюю зарплату по вакансиям"""
 
-    def get_vacancies_with_higher_salary(self):
-        pass
+        # Открытие соединения с базой данных
+        conn = psycopg2.connect(dbname='vacancies', **self.params)
+
+        # Добавление данных в таблицу vacancies
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute("""SELECT ROUND(AVG(salary_from), 1)
+                                   FROM vacancies"""
+                        )
+            for i in cur:
+                data = float(i[0])
+
+        # Закрытие соединения
+        conn.commit()
+        conn.close()
+        return data
+
+    def get_vacancies_with_higher_salary(self) -> list[dict]:
+        """Метод получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+
+        # Открытие соединения с базой данных
+        conn = psycopg2.connect(dbname='vacancies', **self.params)
+
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute("""SELECT vacancies.title,
+                        employer.title,
+                        salary_from,
+                        salary_to,
+                        currency,
+                        vacancies.area,
+                        vacancies.url,
+                        created,
+                        requirement
+                        FROM vacancies
+                        JOIN employer USING(employer_id)
+                        WHERE salary_from >= (SELECT AVG(salary_from) FROM vacancies)"""
+                        )
+            data = []
+            for i in cur:
+                salary = {'from': i[2],
+                          'to': i[3],
+                          'currency': i[4]
+                          }
+                data.append({'aggregator': None,
+                             'name': i[0],
+                             'employer': i[1],
+                             "salary": salary,
+                             'area': i[5],
+                             'created': i[7],
+                             'url': i[6],
+                             'requirement': i[8]
+                             })
+
+        # Закрытие соединения
+        conn.commit()
+        conn.close()
+        return data
 
     def get_vacancies_with_keyword(self):
         pass

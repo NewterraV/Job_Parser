21594bab35573abf9950e49cc4dbe5c0b14db659
Vladislav Poroshin <vld.poroshin@gmail.com>
2023-07-10 18:17:05 +0400

    Создана основная логика для работы программы

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from src.classes.work_with_user import WorkWithUserBase, WorkWithUserKeyWord, WorkWithUserEmployer
+from src.classes.DBmanger import WorkWithDB
 from src.classes.user_exception import CheckExit
+from requests import HTTPError, ConnectionError
 
 
 if __name__ == "__main__":
     user = WorkWithUserBase()
+    bd = WorkWithDB()
+    bd.create_database()
     while True:
         try:
             user_start = user.starting_program()
@@ -22,9 +26,18 @@ if __name__ == "__main__":
             elif user_start == '2':
                 user.create_config_database()
                 continue
+
+            user.job_analysis()
             break
 
+        except (HTTPError, ConnectionError):
+            print('\033[31Неполадки с подключением к серверу:\033[0m'
+                  )
+
         except CheckExit:
+            print('\033[31mВыполняется выход из приложения.\033[0m')
+
+        finally:
             # Предлагаем пользователю повторить поиск
             if user.get_repeat():
                 continue

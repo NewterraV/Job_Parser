98e01d00a9c1d4e65666cdf9132eecce52fb0443
Vladislav Poroshin <vld.poroshin@gmail.com>
2023-07-10 15:41:40 +0400

    Выполнено разделения метода получения информации о вакансиях по работодателю.

--- src/classes/api.py
+++ src/classes/api.py
@@ -50,23 +50,42 @@ class HeadHunterAPI(BaseAPI):
             raise HTTPError(f'Ошибка сервера. Код ответа: {response.status_code}')
         self.__response = response
 
-    def get_employers(self, employers: list) -> list[dict]:
-        """Метод получает информацию о работодателе"""
+    def get_data_employers(self, employers: list) -> list[dict]:
+        """Метод получает информацию о работодателях"""
+
         data_employers = []
         print('\033[32mЗагружаю данные работодателей\033[0m')
-        for employer in tqdm(employers, colour='green'):
+        # Перебираем работодателей запрашивая по каждому информацию
+        for employer in tqdm(employers, bar_format='{l_bar}{bar}{n_fmt}/{total_fmt} [{elapsed}]',
+                             colour='green'):
             response = get(f'{self.__url_employers}{employer}').json()
             data_employers.append({'name': response['name'],
                                    'id': employer,
                                    'url': response['site_url'],
-                                   'area': response['area']['name']})
+                                   'area': response['area']['name']}
+                                  )
         return data_employers
+
+    def get_employer_vacancies(self, employers: list) -> list[dict]:
+        """Метод получает информацию о вакансиях по работодателю"""
+
+        vacancy_employers = []
+        print('\n\033[32mПолучаю данные по вакансиям работодателей\033[0m')
+
+        for employer in employers:
+            self.employer_id = employer
+            all_vacancy = self.get_all_vacancies()
+            vacancy_employers.extend(all_vacancy)
+
+        print(f'Успех, найдено вакансий: {len(vacancy_employers)}')
+
+        return vacancy_employers
 
     def get_vacancies(self) -> list:
         """Возвращает список вакансий на основе атрибута __response"""
 
         vacancies = []
-        for i in tqdm(self.__response.json()['items'], colour='green'):
+        for i in self.__response.json()['items']:
             try:
                 salary = {'from': i['salary']['from'],
                           'to': i['salary']['to'],
@@ -75,8 +94,7 @@ class HeadHunterAPI(BaseAPI):
                                                  'to': 0,
                                                  'currency': None
                                                  }
-                vacancies.append({'aggregator': 'HeadHunter',
-                                  'name': i["name"],
+                vacancies.append({'name': i["name"],
                                   'employer': i['employer']['name'],
                                   'employer_id': i['employer']['id'],
                                   "salary": salary,
@@ -94,14 +112,12 @@ class HeadHunterAPI(BaseAPI):
         """Возвращает максимально возможный список вакансий из API"""
 
         all_vacancies = []
-        count = 0
-        print('\033[32mПолучаю данные по вакансиям HH.ru\033[0m')
-        while count < 20:
-            self.get_response(count)
+        for i in tqdm(range(20), bar_format='{l_bar}{bar}{n_fmt}/{total_fmt} [{elapsed}]',
+                      colour='green'):
+            self.get_response(i)
             # фильтрация бесполезных обращений к API для уменьшения времени выполнения
             if not self.__response.json()['items']:
                 break
-            count += 1
             all_vacancies.extend(self.get_vacancies())
         return all_vacancies
 
@@ -119,7 +135,7 @@ class SuperJobAPI(BaseAPI):
         self.employer_id = employer_id
         self.keyword = keyword
         self.__response = None
-        self.get_response()\
+        self.get_response()
 
     def get_response(self, *args) -> None:
         """Метод делает запрос к API, в качестве аргумента может принимать номер страницы для передачи в API.
@@ -142,8 +158,7 @@ class SuperJobAPI(BaseAPI):
         vacancies = []
         for i in tqdm(self.__response.json()['objects'], colour='green'):
             try:
-                vacancies.append({'aggregator': 'SuperJob',
-                                  'name': i["profession"],
+                vacancies.append({'name': i["profession"],
                                   'employer': i['firm_name'],
                                   "salary": {'from': i['payment_from'],
                                              'to': i['payment_to'],

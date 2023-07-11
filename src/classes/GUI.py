ec8eb738a5f0b7df2a1e26ac9e73cc44f8cab1cc
Vladislav Poroshin <vld.poroshin@gmail.com>
2023-07-11 16:21:20 +0400

    Добавлена проверка на добавление кнопки Exit при генерации.

--- src/classes/GUI.py
+++ src/classes/GUI.py
@@ -1,9 +1,13 @@
 import PySimpleGUI as sg
+from typing import Any
 from src.classes.user_exception import CheckExit
 
 
 class GUI:
     """Класс с методами графической оболочки"""
+
+    def __init__(self):
+        sg.theme('DarkTeal6')
 
     @staticmethod
     def print_message(text, title='Сообщение', color=None):
@@ -20,12 +24,13 @@ class GUI:
         window.close()
 
     @ staticmethod
-    def create_buttons(buttons: list[dict]):
+    def create_buttons(data: dict[str, list[dict[Any]]], check_exit=True):
         """Метод на основе словаря создает окно с кнопками"""
-        layout = [[sg.Text('Выберите функцию:')]]
-        for i in buttons:
+        layout = [[sg.Text(data['name'])]]
+        for i in data['buttons']:
             layout.append([sg.Button(f'{i["title"]}', size=40, key=i['key'], enable_events=True)])
-        layout.append([sg.Cancel('Exit')])
+        if check_exit:
+            layout.append([sg.Cancel('Exit')])
 
         window = sg.Window('Создание DataBase Config', layout)
         event, values = window.read()
